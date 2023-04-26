f68c2b61fdf7cfb40e90ae3fb1da1a6b3d750b41
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-04-26 23:45:39 +0300

    1,1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+const link = list.querySelectorAll("a");
+
+console.log("list :>> ", list);
+// console.log("link :>> ", link);
+// link.forEach((el) => el.preventDefault());
+list.addEventListener("click", imgModal);
+
+function imgModal(evt) {
+  evt.preventDefault();
+  console.log("evt.turget :>> ", evt.target.nodeName);
+  console.log("evt.currentTurget :>> ", evt.currentTarget.class);
+  console.log(evt.target.class);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log("evt :>> ", evt);
+}
